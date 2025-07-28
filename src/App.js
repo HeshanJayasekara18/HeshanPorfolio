321bf7378b5fa13bb82e1723c3de5df2bc831e31
HeshanJayasekara18 <heshanjayasekara18@gmail.com>
2025-07-29 01:42:32 +0530

    aa js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import { HashRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './home/Home';
 import About from './about/About';
 import Contact from './contact/Contactpage';
 
 function App() {
   return (
-    <HashRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </HashRouter>
+    </Router>
   );
 }
 
