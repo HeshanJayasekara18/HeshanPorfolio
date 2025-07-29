eaa9301fa5a455b449444f354a393c1b393f8b86
HeshanJayasekara18 <heshanjayasekara18@gmail.com>
2025-07-29 19:46:43 +0530

    app fix

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
